2d3aec8478004869cfc3ad5b2e7b5ad6173e966d
Oliver Henrichsen <oliver@velanautica.org>
2021-10-24 16:19:28 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,351 @@
+/*************************************************************
+Oliver
+
+20211023
+
+ ESP8266 and ESP32
+
+simple websocket server
+
+Uses
+- webserver connects to WiFi
+- LittleFS with file upload in PlatformIO task
+- platformio.ini is setup for LITTLEFS, good to use as template
+
+************************************************************/
+
+#include <Arduino.h>
+
+#include <ArduinoOTA.h>
+#ifdef ESP32
+#include <FS.h>
+#include <LITTLEFS.h> // need to change LittleFS to LITTLEFS for ESP32
+#include <time.h>
+#include <ESPmDNS.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <LittleFS.h> // need to change LITTELEFS to LittleFS for ESP8266
+#include <FS.h>
+#include <time.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESP8266mDNS.h>
+#endif
+#include <ESPAsyncWebServer.h>
+#include <SPIFFSEditor.h> // eventually this need further adaptation to LittelFS
+
+#define FORMAT_LITTLEFS_IF_FAILED true
+
+#define SERIAL_DEBUG Serial
+
+// SKETCH BEGIN
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+AsyncEventSource events("/events");
+
+const char *ssid = "Vela_Nautica";
+const char *password = "oliverabc123%";
+const char *hostName = "esp-asyncFS";
+const char *http_username = "admin";
+const char *http_password = "admin";
+
+void onWsEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+  if (type == WS_EVT_CONNECT)
+  {
+    Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
+    client->printf("Hello Client %u :)", client->id());
+    client->ping();
+  }
+  else if (type == WS_EVT_DISCONNECT)
+  {
+    Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
+  }
+  else if (type == WS_EVT_ERROR)
+  {
+    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t *)arg), (char *)data);
+  }
+  else if (type == WS_EVT_PONG)
+  {
+    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len) ? (char *)data : "");
+  }
+  else if (type == WS_EVT_DATA)
+  {
+    AwsFrameInfo *info = (AwsFrameInfo *)arg;
+    String msg = "";
+    if (info->final && info->index == 0 && info->len == len)
+    {
+      // the whole message is in a single frame and we got all of it's data
+      Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT) ? "text" : "binary", info->len);
+
+      if (info->opcode == WS_TEXT)
+      {
+        for (size_t i = 0; i < info->len; i++)
+        {
+          msg += (char)data[i];
+        }
+      }
+      else
+      {
+        char buff[3];
+        for (size_t i = 0; i < info->len; i++)
+        {
+          sprintf(buff, "%02x ", (uint8_t)data[i]);
+          msg += buff;
+        }
+      }
+      Serial.printf("%s\n", msg.c_str());
+
+      if (info->opcode == WS_TEXT)
+        client->text("I got your text message");
+      else
+        client->binary("I got your binary message");
+    }
+    else
+    {
+      // message is comprised of multiple frames or the frame is split into multiple packets
+      if (info->index == 0)
+      {
+        if (info->num == 0)
+          Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
+        Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+      }
+
+      Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT) ? "text" : "binary", info->index, info->index + len);
+
+      if (info->opcode == WS_TEXT)
+      {
+        for (size_t i = 0; i < len; i++)
+        {
+          msg += (char)data[i];
+        }
+      }
+      else
+      {
+        char buff[3];
+        for (size_t i = 0; i < len; i++)
+        {
+          sprintf(buff, "%02x ", (uint8_t)data[i]);
+          msg += buff;
+        }
+      }
+      Serial.printf("%s\n", msg.c_str());
+
+      if ((info->index + len) == info->len)
+      {
+        Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+        if (info->final)
+        {
+          Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT) ? "text" : "binary");
+          if (info->message_opcode == WS_TEXT)
+            client->text("I got your text message");
+          else
+            client->binary("I got your binary message");
+        }
+      }
+    }
+  }
+}
+
+// list LITTLEFS directory
+void listDir(fs::FS &fs, const char *dirname, uint8_t levels)
+{
+    Serial.printf("Listing directory: %s\r\n", dirname);
+
+    File root = fs.open(dirname, "r");
+    if (!root)
+    {
+        Serial.println("- failed to open directory");
+        return;
+    }
+    if (!root.isDirectory())
+    {
+        Serial.println(" - not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while (file)
+    {
+        if (file.isDirectory())
+        {
+            Serial.print("  DIR : ");
+
+            Serial.print(file.name());
+            time_t t = file.getLastWrite();
+            struct tm *tmstruct = localtime(&t);
+            Serial.printf("  LAST WRITE: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
+
+            if (levels)
+            {
+                listDir(fs, file.name(), levels - 1);
+            }
+        }
+        else
+        {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("  SIZE: ");
+
+            Serial.print(file.size());
+            time_t t = file.getLastWrite();
+            struct tm *tmstruct = localtime(&t);
+            Serial.printf("  LAST WRITE: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
+        }
+        file = root.openNextFile();
+    }
+}
+
+void setup()
+{
+
+  /*   esp_log_level_set("*", ESP_LOG_DEBUG);    // set all components to WARN level
+    esp_log_level_set("wifi", ESP_LOG_WARN);  // enable WARN logs from WiFi stack
+    esp_log_level_set("dhcpc", ESP_LOG_WARN); // enable INFO logs from DHCP client */
+
+  // ESP32 we use the USB serial interface for console/debug messages
+  SERIAL_DEBUG.begin(115200, SERIAL_8N1);
+  SERIAL_DEBUG.setDebugOutput(true);
+
+  SERIAL_DEBUG.println("Olli Serial Debug active....");
+
+  /*   Serial.begin(115200);
+    delay(400);
+    Serial.println("Serial port active");
+    Serial.setDebugOutput(true); */
+  WiFi.mode(WIFI_AP_STA);
+  // WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  SERIAL_DEBUG.println("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+  SERIAL_DEBUG.println("WiFi local IP: %s ");
+  SERIAL_DEBUG.println(WiFi.localIP().toString().c_str());
+
+  // Send OTA events to the browser
+  ArduinoOTA.onStart([]()
+                     { events.send("Update Start", "ota"); });
+  ArduinoOTA.onEnd([]()
+                   { events.send("Update End", "ota"); });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+                        {
+    char p[32];
+    sprintf(p, "Progress: %u%%\n", (progress/(total/100)));
+    events.send(p, "ota"); });
+  ArduinoOTA.onError([](ota_error_t error)
+                     {
+    if(error == OTA_AUTH_ERROR) events.send("Auth Failed", "ota");
+    else if(error == OTA_BEGIN_ERROR) events.send("Begin Failed", "ota");
+    else if(error == OTA_CONNECT_ERROR) events.send("Connect Failed", "ota");
+    else if(error == OTA_RECEIVE_ERROR) events.send("Recieve Failed", "ota");
+    else if(error == OTA_END_ERROR) events.send("End Failed", "ota"); });
+  ArduinoOTA.setHostname(hostName);
+  ArduinoOTA.begin();
+
+  MDNS.addService("http", "tcp", 80);
+
+  if (!LittleFS.begin()) // FORMAT_LITTLEFS_IF_FAILED
+  {
+    SERIAL_DEBUG.println("LITTLEFS mount failed, did you upload file system image?");
+  }
+  else
+  {
+    SERIAL_DEBUG.println("LITTLEFS mounted, totalBytes=, usedBytes=");
+    // SERIAL_DEBUG.println( LittleFS.totalBytes());
+    // SERIAL_DEBUG.println( LITTLEFS.usedBytes());
+    //  listDir(LittleFS, "/", 0);
+    SERIAL_DEBUG.println("LittleFS was mounted.....");
+  }
+
+  listDir(LittleFS, "/", 5);    // list LITTLEFS directory
+
+  ws.onEvent(onWsEvent);
+  server.addHandler(&ws);
+
+  events.onConnect([](AsyncEventSourceClient *client)
+                   { client->send("hello!", NULL, millis(), 1000); });
+  server.addHandler(&events);
+
+#ifdef ESP32
+  server.addHandler(new SPIFFSEditor(LittleFS, http_username, http_password));
+#elif defined(ESP8266)
+  server.addHandler(new SPIFFSEditor(http_username, http_password));
+#endif
+
+  server.on("/heap", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(200, "text/plain", String(ESP.getFreeHeap())); });
+
+  server.serveStatic("/", LittleFS, "/").setDefaultFile("index.htm");
+
+  server.onNotFound([](AsyncWebServerRequest *request)
+                    {
+    Serial.printf("NOT_FOUND: ");
+    if(request->method() == HTTP_GET)
+      Serial.printf("GET");
+    else if(request->method() == HTTP_POST)
+      Serial.printf("POST");
+    else if(request->method() == HTTP_DELETE)
+      Serial.printf("DELETE");
+    else if(request->method() == HTTP_PUT)
+      Serial.printf("PUT");
+    else if(request->method() == HTTP_PATCH)
+      Serial.printf("PATCH");
+    else if(request->method() == HTTP_HEAD)
+      Serial.printf("HEAD");
+    else if(request->method() == HTTP_OPTIONS)
+      Serial.printf("OPTIONS");
+    else
+      Serial.printf("UNKNOWN");
+    Serial.printf(" http://%s%s\n", request->host().c_str(), request->url().c_str());
+
+    if(request->contentLength()){
+      Serial.printf("_CONTENT_TYPE: %s\n", request->contentType().c_str());
+      Serial.printf("_CONTENT_LENGTH: %u\n", request->contentLength());
+    }
+
+    int headers = request->headers();
+    int i;
+    for(i=0;i<headers;i++){
+      AsyncWebHeader* h = request->getHeader(i);
+      Serial.printf("_HEADER[%s]: %s\n", h->name().c_str(), h->value().c_str());
+    }
+
+    int params = request->params();
+    for(i=0;i<params;i++){
+      AsyncWebParameter* p = request->getParam(i);
+      if(p->isFile()){
+        Serial.printf("_FILE[%s]: %s, size: %u\n", p->name().c_str(), p->value().c_str(), p->size());
+      } else if(p->isPost()){
+        Serial.printf("_POST[%s]: %s\n", p->name().c_str(), p->value().c_str());
+      } else {
+        Serial.printf("_GET[%s]: %s\n", p->name().c_str(), p->value().c_str());
+      }
+    }
+
+    request->send(404); });
+  server.onFileUpload([](AsyncWebServerRequest *request, const String &filename, size_t index, uint8_t *data, size_t len, bool final)
+                      {
+    if(!index)
+      Serial.printf("UploadStart: %s\n", filename.c_str());
+    Serial.printf("%s", (const char*)data);
+    if(final)
+      Serial.printf("UploadEnd: %s (%u)\n", filename.c_str(), index+len); });
+  server.onRequestBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
+                       {
+    if(!index)
+      Serial.printf("BodyStart: %u\n", total);
+    Serial.printf("%s", (const char*)data);
+    if(index + len == total)
+      Serial.printf("BodyEnd: %u\n", total); });
+  server.begin();
+}
+
+void loop()
+{
+  ArduinoOTA.handle();
+  ws.cleanupClients();
+}
